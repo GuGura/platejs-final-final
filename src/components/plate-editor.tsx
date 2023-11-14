b15f56ec8c6ee6eefc829a0b7489255d816b016c
pjy <wodus331@vanillacode.net>
2023-11-14 15:21:11 +0900

    mention

--- src/components/plate-editor.tsx
+++ src/components/plate-editor.tsx
@@ -4,6 +4,8 @@ import {Plate} from "@udecode/plate";
 import {DndProvider} from "react-dnd";
 import {plugins} from "@/lib/plate/plate-plugins";
 import {HTML5Backend} from "react-dnd-html5-backend";
+import {MentionCombobox} from "@/components/plate-ui/mention-combobox";
+import {MENTIONABLES} from "@/lib/plate/mentionables";
 
 const initialValue = [
   {
@@ -19,7 +21,12 @@ export default function PlateEditor() {
   return (
     <DndProvider backend={HTML5Backend}>
       <Plate initialValue={initialValue} plugins={plugins}>
-        <Editor/>
+        <Editor className="px-[96px] py-16"
+                autoFocus
+                focusRing={false}
+                variant="ghost"
+                size="md"/>
+        <MentionCombobox items={MENTIONABLES}/>
       </Plate>
     </DndProvider>
   )
--- src/lib/plate/plate-plugins.ts
+++ src/lib/plate/plate-plugins.ts
@@ -1,14 +1,39 @@
-import { createDndPlugin } from '@udecode/plate-dnd';
-import { createNodeIdPlugin } from '@udecode/plate-node-id';
 import {createPlugins} from "@udecode/plate";
+
+/* Drag
+*  - draggable.tsx
+*  - with-draggables.tsx
+*  - tooltip.tsx
+* */
+import {createNodeIdPlugin} from '@udecode/plate-node-id';
+import {createDndPlugin} from '@udecode/plate-dnd';
 import {withDraggables} from "@/components/plate-ui/with-draggables";
+
+/* Mention
+*  - combobox.tsx
+*  - mention-combobox.tsx
+*  - mention-element.tsx
+*  - mention-input-element.tsx
+*  - mentionables.ts
+* */
+import {createComboboxPlugin} from '@udecode/plate-combobox';
+import {createMentionPlugin, ELEMENT_MENTION, ELEMENT_MENTION_INPUT} from '@udecode/plate-mention';
+import {MentionElement} from "@/components/plate-ui/mention-element";
+import {MentionInputElement} from "@/components/plate-ui/mention-input-element";
 
 export const plugins = createPlugins([
   // ...otherPlugins,
   createNodeIdPlugin(),
   createDndPlugin(),
-],{
-  components:withDraggables({
 
+  /* Mention */
+  createComboboxPlugin(),
+  createMentionPlugin(),
+], {
+  components: withDraggables({
+
+    /* Mention */
+    [ELEMENT_MENTION]: MentionElement,
+    [ELEMENT_MENTION_INPUT]: MentionInputElement,
   })
 });
